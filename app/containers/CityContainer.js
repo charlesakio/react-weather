7db9b3a62c3c5776ea1b6914f054d5f57548663d
Charles Javelona <charlesjavelona@gmail.com>
2016-04-27 06:56:18 -0400

    api testing working

--- app/containers/CityContainer.js
+++ app/containers/CityContainer.js
@@ -2,6 +2,17 @@ var React = require('react');
 var PropTypes = React.PropTypes;
 var City = require('../components/City');
 
+var axios = require('axios');
+var _baseURL = 'http://api.openweathermap.org/data/2.5';
+var _APIKEY = 'a8e29972d260d0df1639bf0d7199f680';
+
+function getCurrentWeather(city) {
+  return axios.get(_baseURL + '/weather?q=' + city + '&appid=' + _APIKEY)
+    .then(function (currentWeather) {
+      console.log(currentWeather.data)
+    })
+}
+
 var CityContainer = React.createClass({
   getDefaultProps: function () {
     return {
@@ -19,10 +30,7 @@ var CityContainer = React.createClass({
   handleSubmitCity: function (e) {
     e.preventDefault();
     var city = this.state.city;
-    console.log(city)
-    //this.setState({
-    //  city: ''
-   // });
+    getCurrentWeather(city)
   },
   handleUpdateCity: function (event) {
     this.setState({
