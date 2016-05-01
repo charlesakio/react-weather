9e0e39ed03f81a62fa5b5e1bb77c76d794596a40
Charles Javelona <charlesjavelona@gmail.com>
2016-05-01 00:52:54 -0400

    Change to gh-pages static files

--- app/index.js
+++
@@ -1,7 +0,0 @@
-// @flow
-const React = require('react');
-const ReactDOM = require('react-dom');
-const toStatic = require('react-dom/server');
-const routes = require('./config/routes');
-
-toStatic(routes, document.getElementById('app'));
